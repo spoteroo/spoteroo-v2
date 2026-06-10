61a48879eeae95f0558dfccbc00a6a8c3f2728e2
spoteroo <hellospoteroo@gmail.com>
2026-06-10 16:56:10 +0530

    Remove debug logs

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-
+console.log(
+  "KEY PREFIX:",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 20)
+);
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
